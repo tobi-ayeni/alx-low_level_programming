18a2d289bad7278ac72f147ac80efd5d00eb4fe3
Rotimi-Ayeni, Oluwatobiloba <rotimiayenioluwatobiloba@gmail.com>
2022-10-14 08:18:11 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-char ch = a;
+char ch = 'a';
 
-while (ch <= z)
+while (ch <= 'z')
 {
-if (ch != e && ch != q)
+if (ch != 'e' && ch != 'q')
 {
 putchar(ch);
 }
 ch++;
 }
-putchar(n);
+putchar('\n');
 return (0);
 }
