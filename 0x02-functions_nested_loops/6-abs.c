994108f5e8c9b6a4bbfa002071a8bd736ad32223
Rotimi-Ayeni, Oluwatobiloba <rotimiayenioluwatobiloba@gmail.com>
2022-10-18 12:48:29 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,21 +1,20 @@
 #include "main.h"
-
-/**
- * _abs - Computes the absolute value of an integer.
- * @n: The integer to be computed.
- *
- * Return: The absolute value of the integer.
- */
-int _abs(int n)
-{
-
-if (n >= 0)
-{
-return (n);
-}
-else
-{
-return (-n);
-}
-
+/**                                                                                                                                             
+ * _abs - Computes the absolute value of an integer.                                                                                            
+ * @n: The integer to be computed.                                                                                                              
+ *                                                                                                                                              
+ * Return: The absolute value of the integer.                                                                                                   
+ */                                                                                                                                             
+int _abs(int n)                                                                                                                                 
+{                                                                                                                                               
+                                                                                                                                                
+if (n >= 0)                                                                                                                                     
+{                                                                                                                                               
+return (n);                                                                                                                                     
+}                                                                                                                                               
+else                                                                                                                                            
+{                                                                                                                                               
+return (-n);                                                                                                                                    
+}                                                                                                                                               
+                                                                                                                                                
 } 
