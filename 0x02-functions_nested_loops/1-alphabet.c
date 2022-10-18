e42628e22032154df856de979a6af253c5ba7d24
Rotimi-Ayeni, Oluwatobiloba <rotimiayenioluwatobiloba@gmail.com>
2022-10-18 05:44:25 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,40 +1,17 @@
-#include  "main.h"
-/**
-*print_alphabet - prints the alphabet,in lowercase
-*/
-
-void print_alphabet(void)
-
-{
-
-int i;
-for (i = 'a'; i <= 'z'; i++)
-
-_putchar(i);
-
-_putchar('\n');
-
-}
 #include "main.h"
-/**
-*print_alphabet_x10 - print 10x the alphabet in lower case
-*/
-
-void print_alphabet_x10(void)
-
-{
-
-int count = 0;
-char j;
-
-for (count++ <= 9)
-{
-for (j = 'a'; j <= 'z'; j++)
-
-_putchar(j);
-
-_putchar('\n');
-
-}
-
+/**                                                                                                                                             
+*print_alphabet - prints the alphabet,in lowercase                                                                                              
+*/                                                                                                                                              
+                                                                                                                                                
+void print_alphabet(void)                                                                                                                       
+                                                                                                                                                
+{                                                                                                                                               
+                                                                                                                                                
+int i;                                                                                                                                          
+for (i = 'a'; i <= 'z'; i++)                                                                                                                    
+                                                                                                                                                
+_putchar(i);                                                                                                                                    
+                                                                                                                                                
+_putchar('\n');                                                                                                                                 
+                                                                                                                                                
 }
