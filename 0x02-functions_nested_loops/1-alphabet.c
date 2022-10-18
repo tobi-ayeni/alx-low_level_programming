f17b39129de096880d37d34192951e1a337fc8f6
Rotimi-Ayeni, Oluwatobiloba <rotimiayenioluwatobiloba@gmail.com>
2022-10-18 05:37:52 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,3 @@
-#include "main.h" 
-void print_alphabet(void)
-
-{
-
-int i;
-for (i = 'a'; i <= 'z'; i++)
-
-_putchar(i);
-
-_putchar('\n');
-
-}
 #include "main.h" /**
 *print_alphabet - prints the alphabet,in lowercase
 */
