deeadfdb2abad154543668a8dff53a603aa57782
Rotimi-Ayeni, Oluwatobiloba <rotimiayenioluwatobiloba@gmail.com>
2022-10-18 12:53:52 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,17 +4,15 @@
  * @n: The integer to be computed.                                                                                                              
  *                                                                                                                                              
  * Return: The absolute value of the integer.                                                                                                   
- */                                                                                                                                             
-int _abs(int n)                                                                                                                                 
-{                                                                                                                                               
-                                                                                                                                                
-if (n >= 0)                                                                                                                                     
-{                                                                                                                                               
-return (n);                                                                                                                                     
-}                                                                                                                                               
-else                                                                                                                                            
-{                                                                                                                                               
-return (-n);                                                                                                                                    
-}                                                                                                                                               
-                                                                                                                                                
-} 
+ */
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+else
+{
+return (-n);
+}
+}
