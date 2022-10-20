041fba6a931dcc815c9ed82b1272f124930b7a1a
Rotimi-Ayeni, Oluwatobiloba <rotimiayenioluwatobiloba@gmail.com>
2022-10-20 06:05:24 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints the numbers from 0-9.
+ */
+void print_numbers(void)
+
+{
+
+int k;
+
+for (k = 0; k <= 9; k++)
+{
+_putchar(k + '0');
+
+}
+
+_putchar('\n');
+
+}
