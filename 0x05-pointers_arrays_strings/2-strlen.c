3fe915a9e3524b952d95cb4e2b6b79b4e50bec87
Rotimi-Ayeni, Oluwatobiloba <rotimiayenioluwatobiloba@gmail.com>
2022-10-24 09:12:37 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: string to evaluate
+ *
+ * Return: The length of @str.
+ */
+
+int _strlen(char *s)
+{
+
+int length;
+length = 0;
+
+while (s[length])
+length++;
+
+return (length);
+
+}
