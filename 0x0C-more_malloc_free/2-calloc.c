f7b404e88a563afaca501abc7d71896e64aeb322
Rotimi-Ayeni, Oluwatobiloba <rotimiayenioluwatobiloba@gmail.com>
2022-11-10 12:27:10 -0800

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - Allocate memory for array of nmemb elements of size bytes
+ * @nmemb: Number of elemnts
+ * @size: Size in bytes of elements
+ *
+ * Return: Pointer to new memory, NULL if it fails
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+void *mem;
+char *filler;
+unsigned int index;
+
+if (nmemb == 0 || size == 0)
+return (NULL);
+
+mem = malloc(size * nmemb);
+
+if (mem == NULL)
+return (NULL);
+
+filler = mem;
+
+for (index = 0; index < (size * nmemb); index++)
+filler[index] = '\0';
+
+return (mem);
+}
